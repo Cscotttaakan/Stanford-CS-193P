b6fcfe70015ab422e8699907b507b5f3cb71e57a
Craig Scott <craigscott@Craigs-MacBook-Air.local>
2018-08-08 14:36:44 -0700

    Added new ThemeChooserViewController

---
+++ Concentration/Concentration/ConcentrationThemeChooserViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ConcentrationThemeChooserViewController.swift
+//  Concentration
+//
+//  Created by Craig Scott on 8/8/18.
+//  Copyright © 2018 Craig Scott. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationThemeChooserViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
