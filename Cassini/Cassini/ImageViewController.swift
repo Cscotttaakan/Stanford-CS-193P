6a3fa5a42d67bb377467f54c2f8a9bd52564f66a
Craig Scott <craigscott@craigs-macbook-air.local>
2018-09-02 09:10:39 -0700

    added url file

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imageURL : URL?{
+        didSet{
+            imageView.image = nil
+            if view.window != nil{
+                fetchImage()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if imageView == nil{
+            fetchImage()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    private func fetchImage(){
+        if let url = imageURL {
+           
+            let urlContents = try? Data(contentsOf: url)
+            
+            if let imageData = urlContents{
+                imageView.image = UIImage(data : imageData)
+            }
+            
+        }
+    }
+    
+    
 
 }
