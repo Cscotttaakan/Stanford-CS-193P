29c97edd758c79981e1a8ba6d89f3b1fc38a6576
Craig Scott <craigscott@Craigs-MacBook-Air.local>
2018-09-09 18:47:37 -0700

    Added tabcontroller

---
+++ EmojiArt/EmojiArt/EmojiArtDocumentTable.swift
@@ -0,0 +1,95 @@
+ //
+//  EmojiArtDocumentTable.swift
+//  EmojiArt
+//
+//  Created by Craig Scott on 9/9/18.
+//  Copyright © 2018 Craig Scott. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtDocumentTable: UITableViewController {
+    
+    var emojiArtDocuments : [String] = ["One","Two","Three"]
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return emojiArtDocuments.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = emojiArtDocuments[indexPath.row]
+        
+        return cell
+    }
+  
+    @IBAction func newEmojiArt(_ sender: UIBarButtonItem) {
+        
+        emojiArtDocuments += ["Untitled".madeUnique(withRespectTo: emojiArtDocuments)]
+        tableView.reloadData()
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            emojiArtDocuments.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if splitViewController?.preferredDisplayMode != .primaryOverlay {
+            splitViewController?.preferredDisplayMode = .primaryOverlay
+        }
+    }
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- EmojiArt/EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArt/EmojiArtViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate , UIScrollViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout  {
+    
+    
 
+    var imageFetcher : ImageFetcher!
+    var emojiArtView = EmojiArtView()
+    @IBOutlet weak var scrollViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var emojiCollectionView: UICollectionView! {
+        didSet {
+            emojiCollectionView.dataSource = self
+            emojiCollectionView.delegate = self
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet{
+            scrollView.minimumZoomScale = 0.1
+            scrollView.maximumZoomScale = 5.0
+            scrollView.delegate = self
+            scrollView.addSubview(emojiArtView)
+        }
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        scrollViewHeight?.constant = scrollView.contentSize.height
+        scrollViewWidth?.constant = scrollView.contentSize.width
+    }
+    
     @IBOutlet weak var dropZone: UIView! {
         didSet{
             dropZone.addInteraction(UIDropInteraction(delegate :  self))
@@ -26,17 +56,62 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
     
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
         
+        imageFetcher = ImageFetcher() { (url , image) in
+            
+            DispatchQueue.main.async {
+                self.emojiArtBackgroundImage  = image
+            }
+            
+        }
+        
+        
         session.loadObjects(ofClass: NSURL.self) { nsurls in
+            
+            if let url = nsurls.first as? URL{
+                self.imageFetcher.fetch(url)
+            }
+            
             
         }
         
         session.loadObjects(ofClass: UIImage.self) { images in
-            
+            if let image = images.first as? UIImage{
+                self.imageFetcher.backup = image
+            }
         }
     }
     
-    @IBOutlet weak var emojiArtView: EmojiArtView!
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return emojiArtView
+    }
     
+    var emojiArtBackgroundImage : UIImage? {
+        get {
+            return emojiArtView.backgroundImage
+        }
+        set {
+            scrollView?.zoomScale = 1.0
+            emojiArtView.backgroundImage = newValue
+            let size = newValue?.size ?? CGSize.zero
+            emojiArtView.frame = CGRect(origin : CGPoint.zero , size : size)
+            scrollView?.contentSize = size
+            scrollViewWidth?.constant = size.width
+            scrollViewHeight?.constant = size.height
+            if let dropZone = self.dropZone, size.width > 0 , size.height > 0{
+                scrollView?.zoomScale = max(dropZone.bounds.size.width / size.width , dropZone.bounds.size.height / size.height)
+            }
+        }
+    }
+    
+    var emojis = "🎟🥉🚙🎲"
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
     
 
 }
